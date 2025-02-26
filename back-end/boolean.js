03f1a7960ecd48e14272991dfc83e8d5c8f097b8
Felipcamargo <146205241+Felipcamargo@users.noreply.github.com>
2025-02-26 10:28:53 -0300

    Add files via upload

---
+++ back-end/boolean.js
@@ -0,0 +1,15 @@
+let estudantes = true;
+let estudiosos = false;
+if(estudantes){
+    console.log("tem direito a meia entrada!");
+} else{
+  
+    console.log("pague inteira!")
+}
+console.log(true == 1);
+console.log(false == 0);
+console.log("1" == 1);
+
+console.log(true === 1);
+console.log(false === 0);
+console.log("1" === 1);
---
+++ back-end/condicao.js
@@ -0,0 +1,13 @@
+// CONDIÇÃO
+/* SE (CONDIÇÃO) {
+ CODIGO AQUI   
+} SE NAO {
+    COIGO AQUI }
+    */
+   let idade = 16
+   let possuiCNH = true
+if(idade = 16 && possuiCNH == true){
+    console.log("Você pode dirigir")
+    }else {
+        console.log("ande de ônibus ou uber")
+    }
